195014e6b35b7cbc8bbd4f23564207a54a7e8f17
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-12-17 21:05:38 +0300

    CreatePhoneNumber

---
+++ js/CreatePhoneNumber.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers) {
+    return `(${numbers.slice(0,3).join("")}) ${numbers.slice(3,6).join("")}-${numbers.slice(6).join("")}`;
+ }
+ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); //, "(123) 456-7890";
+ createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]); //, "(111) 111-1111");
+ createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); //, "(123) 456-7890");
+ 
