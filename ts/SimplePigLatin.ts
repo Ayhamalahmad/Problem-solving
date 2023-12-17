832c37b6fbf9aa324dab234b106ef0787d5a3566
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-12-17 19:06:18 +0300

    Simple Pig Latin

--- ts/SimplePigLatin.ts
+++ ts/SimplePigLatin.ts
@@ -1,10 +1,10 @@
-function pigIt(str: string): string {
+export const pigIt = (a : string) : string =>  {
     const endLe: string = "ay";
     const lettersOnlyRegex: RegExp = /^[A-Za-z\s]+$/;
-    return str.split(" ").map((word: string, index: number): string => {
+    return a.split(" ").map((word: string, index: number): string => {
         const firstLeter: string = word.charAt(0);
         return lettersOnlyRegex.test(word) ? word.slice(1) + firstLeter + endLe : word;
     }).join(" ");
-}
+  }
 console.log(pigIt("Pig latin is cool")); // igPay atinlay siay oolcay
 console.log(pigIt("Hello world !")); // elloHay orldway !
